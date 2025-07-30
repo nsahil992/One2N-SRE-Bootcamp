f4090387829797b080ae668f616bcf30d835e4ab
Sahil Naik <nsahil992@gmail.com>
2025-07-30 11:56:30 +0530

    fix: check error returns on db.Close

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
 func main() {
 	cfg := LoadConfig()
-	db := ConnectDB(cfg)
-	defer db.Close()
+
+	db, err := sql.Open("postgres", BuildDBConnectionString(cfg))
+	if err != nil {
+		log.Fatalf("failed to connect to DB: %v", err)
+	}
+	// Check error when closing
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Printf("failed to close db: %v", err)
+		}
+	}()
+
 	r := SetupRouter(db)
-	r.Run(":" + cfg.Port)
+
+	// Check error when running server
+	if err := r.Run(":" + cfg.Port); err != nil {
+		log.Fatalf("server failed to start: %v", err)
+	}
+}
+
+// BuildDBConnectionString builds PostgreSQL connection string from Config
+func BuildDBConnectionString(cfg Config) string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBSSLMode)
 }
